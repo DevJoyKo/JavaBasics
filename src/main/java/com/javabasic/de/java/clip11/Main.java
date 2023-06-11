d421d06a0befc7190097f769703e6f990f0e2b9f
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-11 17:06:21 +0900

    Java Basic - class - initialization

--- src/main/java/com/javabasic/de/java/clip11/Main.java
+++ src/main/java/com/javabasic/de/java/clip11/Main.java
@@ -6,12 +6,24 @@ public class Main {
         String name;
         String country;
         int age;
+        Person(String name, String country, int age){
+            this.name= name;
+            this.country = country;
+            if ("대한민국".equals(country)){
+                this.age = age + 1;
+            } else {
+                this.age = age;
+            }
+        }
+        Person(){
+
+        }
     }
     public static void main(String[] args){
-        Person minsoo = new Person();
-        minsoo.name = "민수";
-        minsoo.country = "대한민국";
-        minsoo.age = 10;
+        Person minsoo = new Person("민수", "대한민국",20);
+//        minsoo.name = "민수";
+//        minsoo.country = "대한민국";
+//        minsoo.age = 10;
 
         Person paul = new Person();
         paul.name =
