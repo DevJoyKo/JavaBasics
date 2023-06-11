0ae875f21612fec495c9952c6f6fd4c8ff7a0bcc
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-11 17:45:21 +0900

    Java Basic - class - inheritance

---
+++ src/main/java/com/javabasic/de/java/clip11_4/Main.java
@@ -0,0 +1,47 @@
+package com.javabasic.de.java.clip11_4;
+
+public class Main {
+    static class Animal {
+        String name;
+        Animal(String name){
+            this.name = name;
+        }
+        public void cry(){
+            System.out.println(name + " is crying.");
+        }
+    }
+    static class Dog extends Animal {
+
+        String breed;
+
+        Dog(String name, String breed){
+            super(name);
+            this.breed = breed;
+        }
+        public void swim(){
+            System.out.println(name + " is swimming");
+        }
+        public void bark(){
+            System.out.println(name  + "("+breed+") is barking");
+        }
+    }
+    static class Cat extends Animal {
+        Cat(String name) {
+            super(name);
+        }
+    }
+
+    public static void main(String[] args){
+        Dog dog = new Dog("코코", "푸들");
+        dog.cry();
+        dog.bark();
+
+        Animal animal = dog;
+        animal.cry();
+//        animal.bark(); - 자식의 메서드 사용불가
+
+
+
+    }
+
+}
