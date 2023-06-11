cceee5da1334a1b1e48fca6b3a973d42c34736ab
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-11 21:33:22 +0900

    Java Basic - class - abstract class

---
+++ src/main/java/com/javabasic/de/java/clip11_6/Peacock.java
@@ -0,0 +1,9 @@
+package com.javabasic.de.java.clip11_6;
+
+public class Peacock extends Bird{
+
+    @Override
+    boolean flyable(int z) {
+        return false;
+    }
+}
---
+++ src/main/java/com/javabasic/de/java/clip11_6/Pigeon.java
@@ -0,0 +1,9 @@
+package com.javabasic.de.java.clip11_6;
+
+public class Pigeon extends Bird{
+
+    @Override
+    boolean flyable(int z) {
+        return z < 10000;
+    }
+}
