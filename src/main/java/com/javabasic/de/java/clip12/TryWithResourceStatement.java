e72e60384c50e7c457cfef88b9476224142c1256
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-12 23:08:07 +0900

    Java Basic - try-catch - Auto closable

--- src/main/java/com/javabasic/de/java/clip12/TryWithResourceStatement.java
+++ src/main/java/com/javabasic/de/java/clip12/TryWithResourceStatement.java
@@ -7,13 +7,15 @@ import java.nio.charset.StandardCharsets;
 
 public class TryWithResourceStatement {
     public static void main(String[] args) throws IOException {
-        FileOutputStream out = new FileOutputStream("test.txt");
-        try{
+        try(FileOutputStream out = new FileOutputStream("test.txt")){
+            // try 괄호문에 들어갈 수 있는 조건: AutoClosable
+//        try{
             out.write("Hello world!".getBytes(StandardCharsets.UTF_8)); // 메모리에 써짐
             out.flush(); // 파일에 써짐
         } catch (IOException e){
             e.printStackTrace();
         }
-        out.close();
+//        out.close();
+//        이미 fileoutputstream 클래스에는 AutoClosable 인터페이스를 가지고 있음.
     }
 }
