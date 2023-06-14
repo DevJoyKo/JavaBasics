1795b4ed472f837a4430668a9da5dfb3fb6fb3d1
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-14 21:10:34 +0900

    Java Basic - formatter

--- src/main/java/com/javabasic/de/java/clip13/Formatter.java
+++ src/main/java/com/javabasic/de/java/clip13/Formatter.java
@@ -1,4 +1,19 @@
 package com.javabasic.de.java.clip13;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
 public class Formatter {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.of(2022, Month.APRIL, 25, 6, 30);
+        System.out.println(localDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
+
+        System.out.println(localDateTime.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(Locale.KOREA)));
+
+        System.out.println(localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
 }
