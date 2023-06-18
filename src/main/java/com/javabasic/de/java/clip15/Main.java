b096c763cd33a7946278a390c810a1727e3a863e
DevJoyKo <dev.joy.soyoung@gmail.com>
2023-06-18 15:32:15 +0900

    feat - JavaBasic Generic concept

---
+++ src/main/java/com/javabasic/de/java/clip15/Main.java
@@ -0,0 +1,12 @@
+package com.javabasic.de.java.clip15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+
+    }
+}
